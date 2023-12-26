805a50d69f47803ca53e1a3c25b3c4d206af8cc2
Saufi <msaufi2325@gmail.com>
2023-12-26 23:53:51 +0900

    Initial commit

---
+++ structs.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+func (u user) outputUserDetails() {
+	// ...
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	appUser := user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthDate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	// ... do something awesome with that gathered data!
+
+	appUser.outputUserDetails()
+
+}
+
+func getUserData(promtText string) string {
+	fmt.Print(promtText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
